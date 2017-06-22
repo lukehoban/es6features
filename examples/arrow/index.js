5de693a448180158b9ca98d7e5d33a99b6af9f4f
keshav-chaure <keshav.chaure@gmail.com>
2017-06-22 08:33:31 +0530

    added Arrow function examples

--- examples/arrow/index.js
+++ examples/arrow/index.js
@@ -1,4 +1,18 @@
+
+
 console.log("Hi")
+
+//named funciton and anonymous  function
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+sayHello("keshav");
+
+const a=function (name){
+  console.log("Hello ",name);
+}
+a("keshav");
+// arrow funcitons are anonymous  function you can not name them insted you can store in variable
 
 // reqular funciton
 const some=function(x){ return x; }
@@ -15,3 +29,23 @@ const add5=(x)=>x+5;
 
 console.log(some1(10));
 console.log(add5(5));
+
+// regular function
+const fruits = ['Mango', 'Banana', 'Grapes'];
+const fruitsCharLength=fruits.map(function(fruit){ return fruit.length});
+console.log(fruitsCharLength);
+
+//equivalent arrow function
+const fruitsCharLength1=fruits.map((f)=>{ return f.length });
+console.log(fruitsCharLength1);
+
+//equivalent arrow function with implicit return and cury braces removed ie. signle line statement, use curybraces for multi line
+const fruitsCharLength2=fruits.map(f => f.length );
+console.log(fruitsCharLength2);
+
+//no argument function
+const fruitsCharLength3=fruits.map(()=> "same string" );
+console.log(fruitsCharLength3);
+
+const fruitsCharLength4=fruits.map(()=> "same string" );
+console.log(fruitsCharLength4);
